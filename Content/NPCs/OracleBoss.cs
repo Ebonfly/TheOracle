23a8b0a101b6e29f3a4f59284e86934340de2c5d
Ebonfly <73665110+Ebonfly@users.noreply.github.com>
2025-09-20 13:23:46 +0200

    feat: oracle skeleton

---
+++ Content/NPCs/OracleBoss.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace TheOracle.Content.NPCs;
+
+public class OracleBoss : ModNPC
+{
+    public override string Texture => "TheOracle/Assets/Images/NPCs/OracleBoss";
+}
