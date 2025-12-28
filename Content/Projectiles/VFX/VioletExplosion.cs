29d7a20b2e68449f35b5a7bf97ea93efd8cb7131
Ebonfly <73665110+ebonfly@users.noreply.github.com>
2025-12-28 15:01:53 +0100

    feat: roar explosion variant

--- Content/Projectiles/VFX/VioletExplosion.cs
+++ Content/Projectiles/VFX/VioletExplosion.cs
@@ -21,15 +21,15 @@ public class VioletExplosion : ModProjectile
 
     public override void OnSpawn(IEntitySource source)
     {
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i < ((Projectile.ai[2] < 1) ? 50 : 20); i++)
         {
             Color col = Color.Lerp(Color.DarkViolet, Color.Purple, i / 50f);
+            Vector2 vel = Main.rand.NextVector2Circular(35, 35);
             if (i % 2 == 0)
-                Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<SparkleDust>(),
-                    Main.rand.NextVector2Circular(35, 35), newColor: col);
+                Dust.NewDustPerfect(Projectile.Center + vel, ModContent.DustType<SparkleDust>(), vel, newColor: col);
             else
-                Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<GlowDust>(),
-                    Main.rand.NextVector2Circular(15, 15), newColor: col);
+                Dust.NewDustPerfect(Projectile.Center + vel * 0.5f, ModContent.DustType<GlowDust>(), vel * 0.5f,
+                    newColor: col);
         }
     }
 
@@ -52,23 +52,29 @@ public class VioletExplosion : ModProjectile
                 null, Color.DarkViolet with { A = 0 } * 0.15f * Projectile.ai[0], Main.rand.NextFloat(MathHelper.Pi),
                 tex.Size() / 2, Main.rand.NextFloat(0.98f, 1.02f) * Projectile.scale * 4, SpriteEffects.None);
 
-        Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null,
-            Color.White with { A = 0 } * Projectile.ai[0], 0, tex.Size() / 2,
-            Projectile.scale * 2.5f * new Vector2(1, 0.25f), SpriteEffects.None);
+        if (Projectile.ai[2] < 1)
+        {
+            Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null,
+                Color.White with { A = 0 } * Projectile.ai[0], 0, tex.Size() / 2,
+                Projectile.scale * 2.5f * new Vector2(1, 0.25f), SpriteEffects.None);
+        }
 
         tex = Images.Extras.Textures.Glow;
         Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null,
             Color.DarkViolet with { A = 0 } * 0.5f * Projectile.ai[0], 0, tex.Size() / 2,
             Projectile.scale * 4.5f, SpriteEffects.None);
 
+        if (Projectile.ai[2] < 1)
+        {
+            tex = Images.Extras.Textures.Crosslight;
+            for (int i = 0; i < 2; i++)
+                Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null,
+                    (i == 0 ? Color.White : Color.DarkViolet) with { A = 0 } * Projectile.ai[0],
+                    i * MathHelper.PiOver4, tex.Size() / 2,
+                    Projectile.scale * (4.5f - i),
+                    SpriteEffects.None);
+        }
 
-        tex = Images.Extras.Textures.Crosslight;
-        for (int i = 0; i < 2; i++)
-            Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null,
-                (i == 0 ? Color.White : Color.DarkViolet) with { A = 0 } * Projectile.ai[0],
-                i * MathHelper.PiOver4, tex.Size() / 2,
-                Projectile.scale * (4.5f - i),
-                SpriteEffects.None);
         return false;
     }
 }
